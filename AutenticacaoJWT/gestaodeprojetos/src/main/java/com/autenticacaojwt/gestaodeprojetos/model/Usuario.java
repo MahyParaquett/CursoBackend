8fce806a582fac921262c1bcd96658c0c9b4dbdc
MahyParaquett <mahyaraparaquett@gmail.com>
2024-02-19 19:48:30 -0300

    Feat: Classe de serviço do JWTService

---
+++ AutenticacaoJWT/gestaodeprojetos/src/main/java/com/autenticacaojwt/gestaodeprojetos/model/Usuario.java
@@ -0,0 +1,121 @@
+package com.autenticacaojwt.gestaodeprojetos.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+
+
+
+@Entity
+@SequenceGenerator(name="generator_usuario", sequenceName="sequance_usuario", initialValue = 1, allocationSize=1)
+public class Usuario implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "generatr_usuario")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String name, String email, String senha) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Usuario(String name, String email, String senha) {
+        this.name = name;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    //Daqui pra baixo é implementação do userdetails
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+       return senha;
+    }
+
+    @Override
+    public String getUsername() {
+       return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    
+
+}
---
+++ AutenticacaoJWT/gestaodeprojetos/src/main/java/com/autenticacaojwt/gestaodeprojetos/service/JWTService.java
@@ -0,0 +1,87 @@
+package com.autenticacaojwt.gestaodeprojetos.service;
+
+import java.util.Date;
+import java.util.Optional;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import com.autenticacaojwt.gestaodeprojetos.model.Usuario;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JWTService {
+    // chave que é capaz de descriptografar e criptografar o token
+    private static final String chavePrivadaJWT = "secretKey";
+
+    /**
+     * Método para gerar o token JWT
+     * @param autentication Autenticação do usuário
+     * @return Token
+    */
+    public String gerarToken(Authentication authentication) {
+
+        //Aqui pode variar com a regra de negócio
+        // um dia em milisegundos
+        int tempoExpiracao = 86400000; 
+
+        //Criação da data de expiração para o token com base no tempo de expiração. 
+        //Ele pega a data atual e soma mais um dia em milissegundos
+        Date dataExpiracao = new Date(new Date().getTime() + tempoExpiracao);
+
+        //Aqui pegamos o usuario atual da autenticação
+        Usuario usuario = (Usuario)authentication.getPrincipal();
+
+        //Aqui juntamos todos os dados e geramos um token do JWT
+        return Jwts.builder()
+                .setSubject(usuario.getId().toString()) //id do usuario
+                .setIssuedAt(new Date()) //dia de inicio
+                .setExpiration(dataExpiracao) //dia da expiração
+                .signWith(SignatureAlgorithm.HS512, chavePrivadaJWT) //tipo de criptografia e quem lê
+                .compact();
+    }
+
+    /**
+     * Método para retornar o id do usuário dono do token
+     * @param token Token do usuário
+     * @return id
+    */
+    public Optional<Long> obterIdDoUsuario(String token){
+        try {
+            //Retorna as permissões do token
+            Claims claims = parse(token).getBody();
+
+            //Retorna o id de dentro do token se tiver, se não retorna null.
+            return Optional.ofNullable(Long.parseLong(claims.getSubject()));
+            
+        } catch (Exception e) {
+            //Se não encontrar nada, devolve um optional null.
+            return Optional.empty();        
+        }
+    }
+
+    //Método que sabe descobrir de dentro do token com base na chave privada quais são as permissões do usuário.
+    private Jws<Claims> parse(String token) {
+        return Jwts.parser().setSigningKey(chavePrivadaJWT).parseClaimsJws(token);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
