2b4240f75c5d976bd9de44cb4abac33e07dd2952
MahyParaquett <mahyaraparaquett@gmail.com>
2024-02-21 22:16:44 -0300

    feat: criação da classe WebSecurityConfig

---
+++ AutenticacaoJWT/gestaodeprojetos/src/main/java/com/autenticacaojwt/gestaodeprojetos/security/WebSecurityConfig.java
@@ -0,0 +1,66 @@
+package com.autenticacaojwt.gestaodeprojetos.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity 
+public class WebSecurityConfig {
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    private JWTAuthenticationFilter jwtAuthenticationFilter;
+
+  
+    /*
+     * Método que devolve a istância do objeto que sabe devolver o nossso padrão de codificação.
+     * Isso não tem nada a ver com o JWT. 
+     * Aqui será usado para codificar a senha do usuário gerando um hash para salvar no banco.
+     */
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    //configura e retorna um provedor de autenticação, este é responsável por validar as credenciais fornecidas pelo usuário
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(customUserDetailsService);
+
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .cors(Customizer.withDefaults()) //habilita o cors
+            .csrf(csrf -> csrf.disable()) //desabilita o csrf
+            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //define a politica de sessao
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/auth/**", "/h2-console/**", "/roles/**", "/swagger-ui/**", "/v3/api-docs/**", "/actuator/**").permitAll() //define as rotas publicas/abertas
+                    .anyRequest().permitAll()); //demais rotas, nao configuradas acima, so poderao ser acessadas mediante autenticacao
+				
+		
+		http.authenticationProvider(authenticationProvider()); //define o provedor de autenticacao
+
+		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); //define o filtro a ser aplicado no ciclo de vida da requisicao
+		return http.build();
+	}
+
+}
---
+++ AutenticacaoJWT/gestaodeprojetos/src/main/java/com/autenticacaojwt/gestaodeprojetos/security/WebSecurityConfigObsoleta.java
@@ -0,0 +1,88 @@
+package com.autenticacaojwt.gestaodeprojetos.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+// A classe WebSecurityConfigurerAdapter já está obsoleta, por isso os métodos estão com erros
+
+@Configuration
+@EnableWebSecurity //Aqui informo que é uma classe de segurança do WebSecurity
+
+public class WebSecurityConfigObsoleta extends WebSecurityConfigurerAdapter  {
+    
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    private JWTAuthenticationFilter jwtAuthenticationFilter;
+
+    /*
+     * Método que devolve a istância do objeto que sabe devolver o nossso padrão de codificação.
+     * Isso não tem nada a ver com o JWT. 
+     * Aqui será usado para codificar a senha do usuário gerando um hash para salvar no banco.
+     */
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    //Método padrão para configurar o nosso custom que o nosso método de codificar senha.
+    @Override
+        public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        authenticationManagerBuilder
+        .userDetailsService(customUserDetailsService)
+        .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+      // Metodo que tem a configuração global de acessos e permissoes por rotas.
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+     // Parte padrão da configuração, por enquanto ignorar.
+        http
+         .cors().and().csrf().disable()
+         .exceptionHandling()
+         .and()
+         .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .authorizeRequests()
+        
+    //A partir daqui entram as rotas que vão precisar de autenticação ou não
+
+        .antMatchers(HttpMethod.POST, "/api/usuarios", "/api/usuarios/login").permitAll()
+        .anyRequest().authenticated();
+
+    
+         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
